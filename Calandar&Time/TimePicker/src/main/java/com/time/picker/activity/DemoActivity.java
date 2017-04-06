61e93c68c4c4c64211a141fb51f25c6392028c51
yline <957339173@qq.com>
2017-04-06 09:35:58 +0800

    this:更新一些内容 next:continue

--- Calandar&Time/TimePicker/src/main/java/com/time/picker/activity/DemoActivity.java
+++ Calandar&Time/TimePicker/src/main/java/com/time/picker/activity/DemoActivity.java
@@ -1,14 +1,19 @@
 package com.time.picker.activity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.time.picker.R;
 import com.time.picker.wheel.SelectTimeView;
+import com.time.picker.wheel.TimePickerView;
 
 public class DemoActivity extends Activity implements SelectTimeView.OnTimeGotListener, View.OnClickListener
 {
@@ -23,6 +28,12 @@ public class DemoActivity extends Activity implements SelectTimeView.OnTimeGotLi
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		DatePicker datePicker;
+		TimePicker timePicker;
+		TimePickerView timePickerView;
+		TimePickerDialog timePickerDialog;
+		DatePickerDialog datePickerDialog;
 
 		timeView = new SelectTimeView(this);
 		timeView.setOnTimeGotListener(this);
