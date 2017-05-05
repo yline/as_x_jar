bf7c4eec3d84f014b981244eb294c0484bae3325
yline <957339173@qq.com>
2017-05-05 09:58:42 +0800

    this:Butter update next:continue

--- ButterKnife/SampleButter/src/main/java/com/butter/activity/MainApplication.java
+++ ButterKnife/SampleButter/src/main/java/com/butter/activity/MainApplication.java
@@ -2,17 +2,8 @@ package com.butter.activity;
 
 
 import com.yline.application.BaseApplication;
-import com.yline.application.SDKConfig;
 
 public class MainApplication extends BaseApplication
 {
 	public static final String TAG = "SampleButterKnife";
-	
-	@Override
-	protected SDKConfig initConfig()
-	{
-		SDKConfig sdkConfig = new SDKConfig();
-		sdkConfig.setLogFilePath(TAG);
-		return sdkConfig;
-	}
 }
